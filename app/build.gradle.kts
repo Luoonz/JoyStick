c1e34e016f5af73ea584bc9142a9f1e6047cdcbc
Luoonz <81923393+Luoonz@users.noreply.github.com>
2024-06-12 17:15:13 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":joystick"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(project(":joystick"))
 }
